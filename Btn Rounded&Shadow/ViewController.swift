d2b748c825a87ec476c920aa98fcd5ed6b9e8a21
J053Fabi0 <arguellojf1@gmail.com>
2017-07-22 20:00:24 -0600

    RGBA sliders for BackgroundColor were added

--- Btn Rounded&Shadow/ViewController.swift
+++ Btn Rounded&Shadow/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet var opacityLabel: UILabel!
     @IBOutlet var radiusLabel: UILabel!
     
+    @IBOutlet var redRGBALabel: UILabel!
+    @IBOutlet var greenRGBALabel: UILabel!
+    @IBOutlet var blueRGBALabel: UILabel!
+    
+    @IBOutlet var redRGBASlider: UISlider!
+    @IBOutlet var greenRGBASlider: UISlider!
+    @IBOutlet var blueRGBASlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -28,6 +36,33 @@ class ViewController: UIViewController {
     @IBAction func changeRadious(_ sender: UISlider) {
         mainButton.layer.shadowRadius = CGFloat(sender.value)
         radiusLabel.text = String(sender.value)
+    }
+    
+    @IBAction func changeRedRGBA(_ sender: UISlider) {
+        mainButton.backgroundColor = UIColor.init(  red: CGFloat(sender.value/255),
+                                                  green: CGFloat(greenRGBASlider.value/255),
+                                                   blue: CGFloat(blueRGBASlider.value/255),
+                                                  alpha: 1)
+        
+        redRGBALabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func changeGreenRGBA(_ sender: UISlider) {
+        mainButton.backgroundColor = UIColor.init(  red: CGFloat(redRGBASlider.value/255),
+                                                  green: CGFloat(sender.value/255),
+                                                   blue: CGFloat(blueRGBASlider.value/255),
+                                                  alpha: 1)
+        
+        greenRGBALabel.text = String(Int(sender.value))
+    }
+    
+    @IBAction func changeBlueRGBA(_ sender: UISlider) {
+        mainButton.backgroundColor = UIColor.init(  red: CGFloat(redRGBASlider.value/255),
+                                                  green: CGFloat(greenRGBASlider.value/255),
+                                                   blue: CGFloat(sender.value/255),
+                                                  alpha: 1)
+        
+        blueRGBALabel.text = String(Int(sender.value))
     }
 
     override func didReceiveMemoryWarning() {
